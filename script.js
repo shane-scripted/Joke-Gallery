9bbb76b3afd46ed30d96b2113cabc4d04c5cf8a9
shane-scripted <13631290+shane-scripted@users.noreply.github.com>
2019-05-24 10:28:49 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,47 @@
+$("#button1").click(function(){   
+    $("#box1").css("background-color","yellow");
+    $("#joke1").append("<br><p class='answer'>Ten-tickles!</p>");
+    $("#button1").hide();
+});
+
+$("#button2").click(function(){   
+    $("#box2").css("background-color","orange");
+    $("#joke2").append("<br><p class='answer'>An im-pasta!</p>");
+    $("#button2").hide();
+});
+
+$("#button3").click(function(){   
+    $("#joke3").attr("src","https://media.giphy.com/media/ZqlvCTNHpqrio/giphy.gif");
+    $("#box3").css("background-color","#4BA4FE");
+    $("#button3").hide();
+});
+
+$("#button4").click(function(){   
+    $("#box4").css("background-color","#FF7CA4");
+    $("#joke4").append("<br><p class='answer'>He was a fungi!</p>");
+    $("#button4").hide();
+});
+
+$("#button5").click(function(){   
+    $("#box5").css("background-color","#4CF4FF");
+    $("#joke5").append("<br><p class='answer'>She was outstanding in her field!</p>");
+    $("#button5").hide();
+});
+
+$("#button6").click(function(){   
+    $("#joke6").attr("src","https://media.giphy.com/media/bOvFrUeeHz2zC/giphy.gif");
+    $("#box6").css("background-color","#FE4B4B");
+    $("#button6").hide();
+});
+
+$("#button7").click(function(){   
+    $("#box7").css("background-color","#A8FA79");
+    $("#joke7").append("<br><p class='answer'>Fish and ships!</p>");
+    $("#button7").hide();
+});
+
+$("#button8").click(function(){   
+    $("#box8").css("background-color","#EB79FA");
+    $("#joke8").append("<br><p class='answer'>Tooth-hurty!</p>");
+    $("#button8").hide();
+});
